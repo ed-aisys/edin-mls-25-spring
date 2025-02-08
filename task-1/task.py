982eca04b9c816cb773cb30c448dd2a7400e68b9
Daniel Kim <dhjkim2001@gmail.com>
2025-02-08 15:43:36 +0000

    Wrote simple tests

--- task-1/task.py
+++ task-1/task.py
@@ -4,6 +4,7 @@ import cupy as cp
 import numpy as np
 import time
 import json
+import scipy
 from test import testdata_kmeans, testdata_knn, testdata_ann
 # ------------------------------------------------------------------------------------------------
 # Your Task 1.1 code here
@@ -19,18 +20,18 @@ def distance_cosine(X, Y):
     with stream1:
         dot = distance_dot(X, Y)
     with stream2:
-        sum_X = cp.sqrt(cp.sum(cp.sqr(X)))
+        sum_X = cp.sqrt(cp.sum(cp.square(X)))
     with stream3:
-        sum_Y = cp.sqrt(cp.sum(cp.sqr(Y)))
+        sum_Y = cp.sqrt(cp.sum(cp.square(Y)))
 
-    Z = cp.mult(sum_X, sum_Y)
+    Z = cp.multiply(sum_X, sum_Y)
     W = cp.divide(dot, Z)
     U = cp.subtract(1, W)
     return U
 
 def distance_l2(X, Y):
     Z = cp.subtract(X, Y)
-    W = cp.sqr(Z)
+    W = cp.square(Z)
     U = cp.sum(W)
     V = cp.sqrt(U)
     return V
@@ -79,6 +80,42 @@ def our_ann(N, D, A, X, K):
 # Test your code here
 # ------------------------------------------------------------------------------------------------
 
+def test_cosine():
+    X, Y = cp.array([1, 2, 3]), cp. array([4, 5, 6])
+    start = time.time()
+    ours = distance_cosine(X, Y)
+    end = time.time()
+    gold = 1 - torch.cosine_similarity(torch.tensor(X, dtype=float), torch.tensor(Y, dtype=float), dim=0).item()
+    assert cp.isclose([ours], [gold])
+    print("Execution Time: {}".format(end - start))
+
+def test_l2():
+    X, Y = cp.array([1, 2, 3]), cp. array([4, 5, 6])
+    start = time.time()
+    ours = distance_l2(X, Y)
+    end = time.time()
+    gold = cp.linalg.norm(X - Y)
+    assert cp.isclose([ours], [gold])
+    print("Execution Time: {}".format(end - start))
+
+def test_dot():
+    X, Y = cp.array([1, 2, 3]), cp. array([4, 5, 6])
+    start = time.time()
+    ours = distance_dot(X, Y)
+    end = time.time()
+    gold = cp.dot(X, Y)
+    assert cp.isclose([ours], [gold])
+    print("Execution Time: {}".format(end - start))
+
+def test_manhattan():
+    X, Y = cp.array([1, 2, 3]), cp. array([4, 5, 6])
+    start = time.time()
+    ours = distance_manhattan(X, Y)
+    end = time.time()
+    gold = scipy.spatial.distance.cityblock(X.get(), Y.get())
+    assert cp.isclose([ours], [gold])
+    print("Execution Time: {}".format(end - start))
+
 # Example
 def test_kmeans():
     N, D, A, K = testdata_kmeans("test_file.json")
@@ -104,4 +141,11 @@ def recall_rate(list1, list2):
     return len(set(list1) & set(list2)) / len(list1)
 
 if __name__ == "__main__":
-    test_kmeans()
+    print("Cosine Distance Test")
+    test_cosine()
+    print("L2 Distance Test")
+    test_l2()
+    print("Dot Distance Test")
+    test_dot()
+    print("Manhattan Distance Test")
+    test_manhattan()
